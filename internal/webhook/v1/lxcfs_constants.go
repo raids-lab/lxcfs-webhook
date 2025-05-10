128a5fe378f8543abeaa6380bae3f150d23f1477
liyilong <mondaycha@outlook.com>
2025-05-10 16:27:18 +0800

    init: lxcfs-webhook with kubebuilder

---
+++ internal/webhook/v1/lxcfs_constants.go
@@ -0,0 +1,198 @@
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+)
+
+var (
+	// AdmissionWebhook
+	AdmissionWebhookAnnotationMutateKey = "lxcfs-admission-webhook.raids-lab.github.io/mutate"
+	AdmissionWebhookAnnotationStatusKey = "lxcfs-admission-webhook.raids-lab.github.io/status"
+
+	// LXCFS VolumeMounts Template
+	// See https://github.com/lxc/lxcfs for more details.
+	VolumeMountsTemplate = []corev1.VolumeMount{
+		{
+			Name:      "lxcfs-proc-cpuinfo",
+			MountPath: "/proc/cpuinfo",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-diskstats",
+			MountPath: "/proc/diskstats",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-meminfo",
+			MountPath: "/proc/meminfo",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-stat",
+			MountPath: "/proc/stat",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-swaps",
+			MountPath: "/proc/swaps",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-uptime",
+			MountPath: "/proc/uptime",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-slabinfo",
+			MountPath: "/proc/slabinfo",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-pressure-io",
+			MountPath: "/proc/pressure/io",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-pressure-cpu",
+			MountPath: "/proc/pressure/cpu",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-proc-pressure-memory",
+			MountPath: "/proc/pressure/memory",
+			ReadOnly:  true,
+		},
+		{
+			Name:      "lxcfs-sys-devices-system-cpu-online",
+			MountPath: "/sys/devices/system/cpu/online",
+			ReadOnly:  true,
+		},
+		{
+			Name:             "var-lib-lxc",
+			MountPath:        "/var/lib/lxc",
+			ReadOnly:         true,
+			MountPropagation: ptr.To(corev1.MountPropagationHostToContainer),
+		},
+	}
+
+	// LXCFS Volumes Template
+	VolumesTemplate = []corev1.Volume{
+		{
+			Name: "lxcfs-proc-cpuinfo",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/cpuinfo",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-diskstats",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/diskstats",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-meminfo",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/meminfo",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-stat",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/stat",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-swaps",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/swaps",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-uptime",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/uptime",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-slabinfo",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/slabinfo",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-pressure-io",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/pressure/io",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-pressure-cpu",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/pressure/cpu",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-proc-pressure-memory",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/proc/pressure/memory",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "lxcfs-sys-devices-system-cpu-online",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxcfs/sys/devices/system/cpu/online",
+					Type: ptr.To(corev1.HostPathFile),
+				},
+			},
+		},
+		{
+			Name: "var-lib-lxc",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/var/lib/lxc",
+					Type: ptr.To(corev1.HostPathDirectory),
+				},
+			},
+		},
+	}
+
+	// Ignored Namespaces
+	IgnoredNamespaces = []string{
+		metav1.NamespaceSystem,
+		metav1.NamespacePublic,
+		"kube-node-lease",
+	}
+)
---
+++ internal/webhook/v1/pod_webhook.go
@@ -0,0 +1,125 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// nolint:unused
+// log is for logging in this package.
+var podlog = logf.Log.WithName("pod-resource")
+
+// SetupPodWebhookWithManager registers the webhook for Pod in the manager.
+func SetupPodWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).For(&corev1.Pod{}).
+		WithValidator(&PodCustomValidator{}).
+		WithDefaulter(&PodCustomDefaulter{}).
+		Complete()
+}
+
+// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+// +kubebuilder:webhook:path=/mutate--v1-pod,mutating=true,failurePolicy=fail,sideEffects=None,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod-v1.kb.io,admissionReviewVersions=v1
+
+// PodCustomDefaulter struct is responsible for setting default values on the custom resource of the
+// Kind Pod when those are created or updated.
+//
+// NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
+// as it is used only for temporary operations and does not need to be deeply copied.
+type PodCustomDefaulter struct {
+	// TODO(user): Add more fields as needed for defaulting
+}
+
+var _ webhook.CustomDefaulter = &PodCustomDefaulter{}
+
+// Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind Pod.
+func (d *PodCustomDefaulter) Default(ctx context.Context, obj runtime.Object) error {
+	pod, ok := obj.(*corev1.Pod)
+
+	if !ok {
+		return fmt.Errorf("expected an Pod object but got %T", obj)
+	}
+	podlog.Info("Defaulting for Pod", "name", pod.GetName())
+
+	// TODO(user): fill in your defaulting logic.
+
+	return nil
+}
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+// NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
+// Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
+// +kubebuilder:webhook:path=/validate--v1-pod,mutating=false,failurePolicy=fail,sideEffects=None,groups="",resources=pods,verbs=create;update,versions=v1,name=vpod-v1.kb.io,admissionReviewVersions=v1
+
+// PodCustomValidator struct is responsible for validating the Pod resource
+// when it is created, updated, or deleted.
+//
+// NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
+// as this struct is used only for temporary operations and does not need to be deeply copied.
+type PodCustomValidator struct {
+	// TODO(user): Add more fields as needed for validation
+}
+
+var _ webhook.CustomValidator = &PodCustomValidator{}
+
+// ValidateCreate implements webhook.CustomValidator so a webhook will be registered for the type Pod.
+func (v *PodCustomValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	pod, ok := obj.(*corev1.Pod)
+	if !ok {
+		return nil, fmt.Errorf("expected a Pod object but got %T", obj)
+	}
+	podlog.Info("Validation for Pod upon creation", "name", pod.GetName())
+
+	// TODO(user): fill in your validation logic upon object creation.
+
+	return nil, nil
+}
+
+// ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type Pod.
+func (v *PodCustomValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	pod, ok := newObj.(*corev1.Pod)
+	if !ok {
+		return nil, fmt.Errorf("expected a Pod object for the newObj but got %T", newObj)
+	}
+	podlog.Info("Validation for Pod upon update", "name", pod.GetName())
+
+	// TODO(user): fill in your validation logic upon object update.
+
+	return nil, nil
+}
+
+// ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type Pod.
+func (v *PodCustomValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	pod, ok := obj.(*corev1.Pod)
+	if !ok {
+		return nil, fmt.Errorf("expected a Pod object but got %T", obj)
+	}
+	podlog.Info("Validation for Pod upon deletion", "name", pod.GetName())
+
+	// TODO(user): fill in your validation logic upon object deletion.
+
+	return nil, nil
+}
